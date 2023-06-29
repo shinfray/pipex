ab4209afda8efe8a8b8927aa0ee09a44b76524c2
shinfray <shinfray@student.s19.be>
2023-06-29 12:15:17 +0200

    update

--- src/set_pipex.c
+++ src/set_pipex.c
@@ -6,7 +6,7 @@
 /*   By: shinfray <shinfray@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 19:15:00 by shinfray          #+#    #+#             */
-/*   Updated: 2023/06/29 01:54:24 by shinfray         ###   ########.fr       */
+/*   Updated: 2023/06/29 12:11:05 by shinfray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,35 @@ void		ft_set_pipex(t_pipex *pipex, int argc, char **argv, char **envp);
 static char	**ft_get_path(char **envp);
 static void	*ft_set_backslash(char **path);
 
+void	ft_get_files(int argc, char **argv, t_pipex *pipex)
+{
+	pipex->outfile = argv[argc - 1];
+	if (pipex->here_doc == 0)
+		pipex->fd_out = open(pipex->outfile, \
+			O_CREAT | O_WRONLY | O_TRUNC, 0644);
+	else
+		pipex->fd_out = open(pipex->outfile, \
+			O_CREAT | O_WRONLY | O_APPEND, 0644);
+	if (pipex->fd_out == -1)
+		perror("pipex");
+	if (pipex->here_doc == 0)
+	{
+		pipex->infile = argv[1];
+		pipex->fd_in = open(pipex->infile, O_RDONLY);
+	}
+	else
+		; //heredoc function
+	if (pipex->fd_in == -1)
+		perror("pipex");
+
+}
+
 void	ft_set_pipex(t_pipex *pipex, int argc, char **argv, char **envp)
 {
 	pipex->argc = argc;
 	pipex->argv = argv;
 	pipex->envp = envp;
 	pipex->path = ft_get_path(envp);
-	pipex->infile = argv[1];
-	pipex->outfile = argv[argc - 1];
 	pipex->args = NULL;
 	pipex->path_cmd = NULL;
 	pipex->fds = NULL;
@@ -31,13 +52,7 @@ void	ft_set_pipex(t_pipex *pipex, int argc, char **argv, char **envp)
 	pipex->total_pipes = pipex->argc - 4;
 	pipex->total_cmds = pipex->total_pipes + 1;
 	pipex->pid_last = -1;
-	pipex->fd_in = open(pipex->infile, O_RDONLY);
-	if (pipex->fd_in == -1)
-		perror("pipex");
-	pipex->fd_out = open(pipex->outfile, \
-		O_CREAT | O_WRONLY | O_TRUNC, 0644);
-	if (pipex->fd_out == -1)
-		perror("pipex");
+	ft_get_files(argc, argv, pipex);
 }
 
 static char	**ft_get_path(char **envp)
