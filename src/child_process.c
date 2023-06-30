4f054672d50843cb2d75fb1db7eea49b6e623e3c
shinfray <shinfray@student.s19.be>
2023-06-30 13:34:42 +0200

    refactoring

--- src/child_process.c
+++ src/child_process.c
@@ -6,7 +6,7 @@
 /*   By: shinfray <shinfray@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/29 01:18:58 by shinfray          #+#    #+#             */
-/*   Updated: 2023/06/30 13:22:10 by shinfray         ###   ########.fr       */
+/*   Updated: 2023/06/30 13:33:29 by shinfray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,39 +28,36 @@ void	ft_createpipe(t_pipex *pipex)
 
 void	ft_exec_childs(t_pipex *pipex)
 {
-	pid_t	pid;
-	int	fd_to_read;
-	int	fd_to_write;
+	const int	fd_to_read = pipex->pipe[0];
+	pid_t		pid;
+	int			fd_to_write;
 
-	fd_to_read = pipex->pipe[0];
 	if (pipex->cmd_index < pipex->total_cmds - 1)
 		ft_createpipe(pipex);
 	else
 		pipex->pipe[1] = pipex->fd_out;
 	fd_to_write = pipex->pipe[1];
 	pid = fork();
-	if (pid != 0)
-		ft_close(2, fd_to_read, fd_to_write);
+	if (pid == 0)
+	{
+		ft_redirect(fd_to_read, fd_to_write, pipex);
+		if (pipex->cmd_index < pipex->total_cmds -1)
+			close(pipex->pipe[0]);
+		ft_parse_args(pipex, pipex->argv[2 + pipex->cmd_index + pipex->here_doc]);
+		execve(pipex->path_cmd, pipex->args, pipex->envp);
+		ft_putstr_fd("pipex: command not found: ", STDERR_FILENO);
+		ft_putendl_fd(pipex->args[0], STDERR_FILENO);
+		pipex->exit_status = COMMAND_NOT_FOUND;
+		ft_quit(pipex);
+	}
+	ft_close(2, fd_to_read, fd_to_write);
 	if (pid < 0)
 	{
 		pipex->exit_status = EXIT_FAILURE;
 		perror("fork error");
 	}
-	if (pid != 0)
-	{
-		if (pipex->cmd_index == pipex->total_cmds - 1)
-			pipex->pid_last = pid;
-		return ;
-	}
-	ft_redirect(fd_to_read, fd_to_write, pipex);
-	if (pipex->cmd_index < pipex->total_cmds -1)
-		close(pipex->pipe[0]);
-	ft_parse_args(pipex, pipex->argv[2 + pipex->cmd_index + pipex->here_doc]);
-	execve(pipex->path_cmd, pipex->args, pipex->envp);
-	ft_putstr_fd("pipex: command not found: ", STDERR_FILENO);
-	ft_putendl_fd(pipex->args[0], STDERR_FILENO);
-	pipex->exit_status = COMMAND_NOT_FOUND;
-	ft_quit(pipex);
+	if (pipex->cmd_index == pipex->total_cmds - 1)
+		pipex->pid_last = pid;
 }
 
 void	ft_wait(t_pipex *pipex)
