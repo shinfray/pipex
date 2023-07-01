19ed3a7fd96fa11f4f2b327364a03901ed054f6e
shinfray <simon@hinfray.eu>
2023-07-01 19:37:18 +0200

    fix: reproduce behavior of zsh when ctrl-D is used whith heredoc

--- src/here_doc.c
+++ src/here_doc.c
@@ -6,21 +6,20 @@
 /*   By: shinfray <shinfray@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/29 15:01:31 by shinfray          #+#    #+#             */
-/*   Updated: 2023/06/29 17:15:29 by shinfray         ###   ########.fr       */
+/*   Updated: 2023/07/01 19:34:54 by shinfray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
 int			ft_here_doc(char **argv);
-static int	ft_here_doc_loop(char *line, int fd_hd, char *limiter);
+static void	ft_here_doc_loop(char *line, int fd_hd, char *limiter);
 
 int	ft_here_doc(char **argv)
 {
 	const char	*path = ".here_doc_temp_file";
 	int			fd_hd_origin;
 	int			fd_hd;
-	int			loop_error_status;
 	char		*line;
 
 	fd_hd_origin = open(path, O_WRONLY | O_CREAT | O_EXCL | O_TRUNC, 0600);
@@ -30,32 +29,30 @@ int	ft_here_doc(char **argv)
 	line = NULL;
 	if (fd_hd != -1)
 	{
-		loop_error_status = ft_here_doc_loop(line, fd_hd, argv[2]);
+		ft_here_doc_loop(line, fd_hd, argv[2]);
 		close(fd_hd);
-		if (loop_error_status == 0)
-			fd_hd = open(path, O_RDONLY);
-		else
-			fd_hd = -1;
+		fd_hd = open(path, O_RDONLY);
 	}
 	close(fd_hd_origin);
 	unlink(path);
 	return (fd_hd);
 }
 
-static int	ft_here_doc_loop(char *line, int fd_hd, char *limiter)
+static void	ft_here_doc_loop(char *line, int fd_hd, char *limiter)
 {
+	ft_putstr_fd("heredoc> ", STDOUT_FILENO);
 	while (1)
 	{
-		ft_putstr_fd("heredoc> ", STDOUT_FILENO);
+		if (line != NULL)
+			free(line);
 		line = get_next_line(STDIN_FILENO);
 		if (line == NULL)
-			return (-1);
+			continue;
 		if (ft_strncmp(line, limiter, ft_strlen(line)) == ('\n' - '\0'))
 			break ;
 		ft_putstr_fd(line, fd_hd);
-		free(line);
+		if (line[ft_strlen(line) - 1] == '\n')
+			ft_putstr_fd("heredoc> ", STDOUT_FILENO);
 	}
-	if (line != NULL)
-		free(line);
-	return (0);
+	free(line);
 }
